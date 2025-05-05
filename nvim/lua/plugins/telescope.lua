a692fc19e970b3832ecd032514ecd2e9c843f9e4
Sam <d.maitreya@vardhamanglobal.net>
2025-05-05 13:02:18 +0530

    nvim config

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return
+{
+    -- Fuzzy Finder (files, lsp, etc)
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+
+      -- Useful for getting pretty icons, but requires a Nerd Font.
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    }
+}
